7ab0a07eedcff904ba5e906a88a332c3f4d31ed0
Lhrfr3445rg4 <92609560+Lhrfr3445rg4@users.noreply.github.com>
2025-03-13 10:57:30 +1200

    Update index.js

--- files/index.js
+++ files/index.js
@@ -6,8 +6,8 @@ const UUID = process.env.UUID || '9af8c03c-ac3d-473c-9ca1-269de815ff02'; //若�
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '5ddVS93Eq0Uc9he880';
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo-0313.qaass.eu.org';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZWVjNjMwMDZjZTYzMjg3MTQ5OTQyMWIyNzZhMGY0NzgiLCJ0IjoiYzVkNzllNDUtOWQ5YS00MTRhLTlhN2QtODA4OGQ0YTkzMmIwIiwicyI6Ik1HWmhPREk1TkRrdFkySTRPQzAwTTJNeUxUaGlNakV0T0RVd1pHVXdZV1prT1RBMSJ9';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo0313.qaass.eu.org';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZWVjNjMwMDZjZTYzMjg3MTQ5OTQyMWIyNzZhMGY0NzgiLCJ0IjoiZTUwODM3MDktODg3OC00NDIwLThhZWQtMjBlZGJkMTI2NDVhIiwicyI6Ik5XRmxNbVUxWVRndE56RXlNaTAwTmpJeExXSTNNMll0TmpBMk9HVXpZakF4Tm1aaiJ9';
 const CFIP = process.env.CFIP || 'na.ma';
 const NAME = process.env.NAME || 'Choreo';
 
